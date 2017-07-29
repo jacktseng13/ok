c4011001fba2ac9e8719b4ab8cd602364d4e8e6a
jacktseng13 <jacktseng13@gmail.com>
2017-07-29 15:15:11 +0800

    add rest files

---
+++ db/migrate/20170729070909_create_pens.rb
@@ -0,0 +1,10 @@
+class CreatePens < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pens do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
